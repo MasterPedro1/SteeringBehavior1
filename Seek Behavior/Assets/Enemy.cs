821abf6f9c60ce5b6dfba55d3be4d67eae45c7ed
LeinadDH <daniel_navarrete_05@hotmail.com>
2023-03-06 16:55:21 -0600

    Añadiendo Pursuit

--- Seek Behavior/Assets/Enemy.cs
+++ Seek Behavior/Assets/Enemy.cs
@@ -2,57 +2,184 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Runtime.ConstrainedExecution;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private Vector3 velocity;
     [SerializeField] private Transform target;
+    [SerializeField] private Transform sprite;
+    [SerializeField] private float maxspeed;
     [SerializeField] private float speed;
+    [SerializeField] private float cirdist;
+    [SerializeField] private float time = 5;
+    [SerializeField] private float time2 = 1;
+    [SerializeField] private float angle;
+    [SerializeField] private float T = 2;
+
+    public PLayer player;
+
+    float circleradius;
+    private Vector3 targetWander;
 
     private Vector3 steering;
 
+    private Queue<Vector3> targetsQueue = new Queue<Vector3>();
+
+
+
     private enum SteeringType
     {
-        Seek, RunAway, Arrival
+        Seek, RunAway, Arrival, Wander, Pursuit
     }
 
     [SerializeField] private SteeringType type = SteeringType.Seek;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    [SerializeField] private float slowingRadios;
+
     //Son delegados que no aceptan ningun parametro uelven ningun valor
 
-    // Start is called before the first frame update
+
     void Start()
     {
+        targetWander = transform.position;
+
         actions.Add("Seek", CalculateSeek);
-        actions.Add("RunAway",CulateRunAway);
+        actions.Add("RunAway", CalculateRunAway);
+        actions.Add("Arrival", CalculateArrival);
+        actions.Add("Wander", CalculateWander);
+        actions.Add("Pursuit", CalculatePursuit);
+        FillQueue();
+
+        
+
+        StartCoroutine(CountdownCoroutine());
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(actions.TryGetValue(type.ToString(), out Action action))
+        if (actions.TryGetValue(type.ToString(), out Action action))
         {
             action();
         }
+
+       
+    }
+
+    void Move(Vector3 steering)
+    {
+        velocity += steering;
+        velocity = Vector3.ClampMagnitude(velocity, maxspeed);
+        transform.position += velocity * Time.deltaTime;
     }
 
     void CalculateSeek()
     {
-        Vector3 desvel = (target.position - transform.position).normalized * speed;
+        steering = Seek(target.position);
 
+        Move(steering);
+    }
+
+    Vector3 Seek(Vector3 target)
+    {
+        Vector3 desvel = (target - transform.position).normalized * maxspeed;
+
+        return desvel - velocity;
+    }
+
+    void CalculateRunAway()
+    {
+        steering = Seek(target.position);
+
+        Move(steering * -1);
+        
+    }
+
+    void CalculateArrival()
+    {
+
+        Vector3 desvel = (target.position - transform.position);
+        float distance = desvel.magnitude;
+
+        if (distance < slowingRadios)
+        {
+            steering = desvel - velocity * (distance / slowingRadios);
+        }
+        else
+        {
+            steering = desvel - velocity;
+        }
         steering = desvel - velocity;
-
         velocity += steering;
         transform.position += velocity * Time.deltaTime;
     }
 
-    void CulateRunAway()
+    void CalculateWander()
     {
-        Vector3 desvel = (target.position - transform.position).normalized * speed;
+        Vector3 target2 = targetWander;
+        steering = Seek(target2);
+        Move(steering);
 
-        steering = desvel - velocity;
+        Vector3 circlecent = velocity.normalized * cirdist;
 
-        velocity += steering;
-        transform.position -= velocity * Time.deltaTime;
+        //Quaternion representa la rotacion de un angulo
+        
+        Vector3 displacement = Vector3.forward * circleradius;
+        Quaternion rotate = Quaternion.AngleAxis(angle, displacement);
+        displacement= rotate * displacement;
+
+        Vector3 wanderforce = circlecent + displacement;
+
+        Vector3 circlepos = transform.position + circlecent;
+
+
+        Debug.DrawLine(transform.position, transform.position + circlecent,Color.red);
+        Debug.DrawLine(circlepos, circlepos + displacement, Color.blue);
+    }
+        
+    void CalculatePursuit()
+    {
+        Vector3 puruit = player.transform.position + (player.m_prevPosition * T);
+
+        steering = Seek(puruit);
+        Move(steering);
+    }
+
+
+
+    private void FillQueue()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 randomTarget = new Vector3(UnityEngine.Random.Range(-8f, 8f), (UnityEngine.Random.Range(-5f, 5f)), 0);
+            targetsQueue.Enqueue(randomTarget);
+            //Sprite.transform.position = randomTarget;
+        }
+    }
+
+    IEnumerator CountdownCoroutine()
+    {
+
+        while (true)
+        {
+
+            yield return new WaitForSeconds(time);
+
+
+            if (targetsQueue.Count == 0) FillQueue();
+
+            targetWander = targetsQueue.Dequeue();
+            sprite.position = targetWander;
+
+        }
+
+    }
+
+    IEnumerator ChangeAngle()
+    {
+        yield return new WaitForSeconds(time2);
+        float angle = UnityEngine.Random.Range(100f, 50f);
+
     }
 }
--- Seek Behavior/Assets/PLayer.cs
+++ Seek Behavior/Assets/PLayer.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PLayer : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public Vector3 m_prevPosition;
 
-   
+
 
     void Start()
     {
+        m_prevPosition = transform.position;
         
     }
 
@@ -18,5 +20,15 @@ public class PLayer : MonoBehaviour
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = Vector3.Lerp(transform.position, new Vector3(mousePosition.x, mousePosition.y,0), moveSpeed * Time.deltaTime);
+
+       
+    }
+
+    public void Pursuit()
+    {
+        var actualVelocity = Vector3.Distance(m_prevPosition, transform.position);
+        actualVelocity /= Time.deltaTime;
+
+        m_prevPosition = transform.position;
     }
 }
